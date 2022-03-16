252caf5a2394035c0f66ce4e300e82aeb151c6e9
Ablett26 <liam.ablett@ntlworld.com>
2022-03-16 14:28:15 +0000

    Built header nav

--- wp-content/themes/punch-test/header.php
+++ wp-content/themes/punch-test/header.php
@@ -5,18 +5,48 @@
  *
  */
 
+ $header_logo = get_field('header_logo', 'options');
+ $header_text = get_field('header_text', 'options');
+
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> >
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php wp_head(); ?>
-</head>
+<html <?php language_attributes(); ?>>
 
-<body <?php body_class(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+  </head>
 
 
-<header>
 
-</header><!-- .site-header -->
+
+  <body <?php body_class(); ?>>
+
+
+    <header>
+      <nav class="header-navigation">
+        <div class="header-navigation__left">
+          <img src="<?php echo $header_logo['url'];?>" alt="<?php echo $header_logo['title'];?>">
+          <p><?php echo $header_text;?></p>
+        </div>
+        <div class="header-navigation__right">
+          <?php
+			echo wp_nav_menu([
+					'theme_location' => 'primary',
+					'menu_class' => 'header-navigation__menu',
+					'container' => false
+			]);
+			?>
+          <div class="burger-menu">
+            <div class="burger-menu__bar bar1"></div>
+            <div class="burger-menu__bar bar2"></div>
+            <div class="burger-menu__bar bar3"></div>
+
+          </div>
+        </div>
+
+      </nav>
+
+
+    </header><!-- .site-header -->
--- wp-content/themes/punch-test/index.php
+++ wp-content/themes/punch-test/index.php
@@ -12,17 +12,17 @@ get_header(); ?>
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
-			echo "index.php file fired";
+			// echo "index.php file fired";
 
 		// End the loop.
 		endwhile;
 
 	else :
-		echo "index.php file else";
+		// echo "index.php file else";
 
 	endif;
 	?>
 
-<?php get_sidebar(); ?>
+<!-- <?php get_sidebar(); ?> -->
 
 <?php get_footer(); ?>
